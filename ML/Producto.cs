1619f9dd401155e868ab749e025ca601699708f7
CRHernandez14 <chrisrhernandezp@gmail.com>
2023-10-05 10:48:46 -0600

    Modelos Producto, nueva rama.

---
+++ ML/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Producto
+    {
+        public Guid IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string FechaRegistro { get; set; }
+        public byte[] Imagen { get; set; }
+
+
+        //Propiedades de navegacion
+        public User User { get; set; }
+        public Tipo Tipo { get; set; }
+        public Proveedor Proveedor { get; set; }
+    }
+}
---
+++ ML/ProductoPiso.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class ProductoPiso
+    {
+        public int IdProductoPiso { get; set; }
+        public Producto Producto { get; set; }
+        public Piso Piso { get; set; }
+    }
+}
